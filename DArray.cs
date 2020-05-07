4aebe9d7606b3532818462811b196eecf78662fe
regzima <64033419+regzima@users.noreply.github.com>
2020-05-07 23:31:20 +0300

    Add files via upload

---
+++ DArray.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Task_1
+{
+    internal class DArray<datatype>
+    {
+        private int size;
+        private int a;
+        private datatype[] array;
+
+        //1. Создаём конструктор без параметров:
+        public DArray()
+        {
+            array = new datatype[10];
+            size = 10;
+        }
+
+        //2. Создаём конструктор с одним целочисленным параметром
+        public DArray(int n)
+        {
+            if (n <= 0)
+            { n = 3; }
+            array = new datatype[n];
+            size = n;
+        }
+
+        //3. Создаём конструктор, который принимает в качестве параметра длину массива
+        public DArray(datatype[] arr)
+        {
+            array = new datatype[arr.Length];
+            size = arr.Length;
+        }
+
+        //4. Создаём метод, который позволяет добавить элемент в конец массива
+        public void EndAddElements(datatype item)
+        {
+            Edit(a); array[a] = item; a++;
+        }
+
+        //5. Создаём метод, который позволяет добавить перечисление в конец массива
+        public void WidthAddElements(datatype[] x)
+        {
+            int n = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                EditArray(a, n, x.Length);
+                array[a] = x[n];
+                a++; n++;
+            }
+        }
+
+        //6. Создаём метод, который позволяем поменять размер массива
+        private void Edit(int v)
+        {
+            if (size <= v)
+            {
+                while (size <= v) { PlusSize(); }
+                datatype[] k = new datatype[size];
+                for (int i = 0; i < array.Length; i++) { k[i] = array[i]; }
+                array = k;
+            }
+        }
+
+        //7. Создаём метод, который позволяет добавить элементы из массива в массив
+        private void EditArray(int v, int count, int length)
+        {
+            if (size <= v)
+            {
+                while (size <= v) { PlusSizeArray(count, length); }
+                datatype[] temparray = new datatype[size];
+                for (int i = 0; i < array.Length; i++) { temparray[i] = array[i]; }
+                array = temparray;
+            }
+        }
+
+        //8. Создаём метод, который добавляет три элемента к массиву, если нет мест
+        private void PlusSize()
+        {
+            size += 3;
+        }
+        private void PlusSizeArray(int count, int length)
+        {
+            size += length - count;
+        }
+        //8. Создаём метод, который удаляет элемент из массива
+        public bool DeleteElement(datatype item)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    IndexOfArray(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //9. Находим индекс элемента
+        private void IndexOfArray(int v)
+        {
+            if (v>=a) { throw new IndexOutOfRangeException(); }
+            int ind = v+1;
+            if (ind<a)
+            {
+                datatype[] k = new datatype[size];
+                Array.Copy(array, 0, k, 0, ind);
+                Array.Copy(array, ind, k, v, a-ind);
+                array = k;
+            } a--; //пустые элементы не имеют индекса
+        }
+
+        //10. Создаём метод вставки элемента в массив
+        public bool CtrlCCtrlV(datatype item, int v)
+        {
+            if (v >= size) { throw new IndexOutOfRangeException(); }
+            datatype[] k = new datatype[size];
+            int length = k.Length;
+            Array.Copy(array, 0, k, 0, v);
+            k[v] = item;
+            for (int i = v + 1; i <= length; i++)
+            {
+                if (k.Length - (a + 1) < 0)
+                {
+                    PlusSize();
+                    datatype[] t = new datatype[size];
+                    for (int j = 0; j < k.Length; j++) 
+                    { 
+                        t[j] = k[j]; 
+                    }
+                    k = t;
+                }
+                try { k[i] = array[i - 1]; }
+                catch{}
+            }
+            array = k;
+            a++;
+            return true;
+        }
+
+        //11. Ищем длину заполненного массива
+        public int Length()
+        { return a; }
+
+        //12. Ищем длину всего массива
+        public int Capacity()
+        { return array.Length; }
+
+        //13. Инициализируем динамический массив
+        public datatype this[int i]
+        { get { return array[i]; } set { array[i] = value; } }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //1. Создаём динамический массив и массив, из которого будем вставлять элементы
+            int[] Array = new int[3] {3,2,1};
+            DArray<int> DArray = new DArray<int>(10);
+
+            Console.WriteLine("РАБОТАЕМ С МАССИВОМ");
+            Console.WriteLine("");
+            Console.WriteLine("1. Создаём динамический массив");
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+
+            //2. Заполняем массив троечками, а три последних элемента оставляем пустыми
+            for (int i = 0; i < DArray.Capacity() - 3; i++)
+            {
+                DArray.EndAddElements(3);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("2. Заполняем массив троечками, кроме трёх последних элементов");
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+            Console.WriteLine("");
+            //3. В начало массива добавляем 1
+            Console.WriteLine("3. Добавляем единичку в начало");
+            DArray.CtrlCCtrlV(1, 0);
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+            Console.WriteLine("");
+            //4. В середину массива добавляем 6
+            Console.WriteLine("4. В середину массива добавляем 6");
+            DArray.CtrlCCtrlV(6, 5);
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+            Console.WriteLine("");
+            //5. Добавляем 9 в конец массива
+            Console.WriteLine("5. Добавляем 9 в конец массива");
+            DArray.EndAddElements(9);
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+            Console.WriteLine("");
+            //6. Добавляем содержимое массива parr
+            Console.WriteLine("6. Добавляем содержимое массива Array");
+            DArray.WidthAddElements(Array);
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+            Console.WriteLine("");
+            //7. Удаляем из массива 3
+            Console.WriteLine("7. Удаляем из массива ВСЕ 3");
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            DArray.DeleteElement(3);
+            for (int i = 0; i < DArray.Capacity(); i++)
+            {
+                Console.Write($" {DArray[i]}");
+            }
+            Console.WriteLine("");
+            //8. Итоговые просчеты: число заполненных элементов и общее число элементов в массиве
+            Console.Write("8. В массиве заполнено элементов - ");
+            Console.Write($"{DArray.Length()}");
+            Console.Write(", всего элементов в массиве - ");
+            Console.Write($"{DArray.Capacity()}");
+            Console.Write(". Конец!");
+            Console.ReadKey();
+        }
+    }
+}
+
